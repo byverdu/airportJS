0269c4895dc3f4d46270a7455d63fc1d0f72c1e6
byverdu <byverdu@gmail.com>
2014-10-18 23:16:26 +0100

    initialization airport tests passing, added Plane class file

--- test/airportSpec.js
+++ test/airportSpec.js
@@ -1,2 +1,27 @@
 var expect  = require('chai').expect
-var Airport = require('../lib/Airport') 
+var Airport = require('../lib/Airport')
+var Plane   = require('../lib/Plane')
+
+var gatwick;
+
+before(function(){
+	gatwick = new Airport("Gatwick")
+});
+
+describe('Airport', function() {
+ 	
+ 	context('When is initialized', function(){
+
+ 		it('Has a name', function() {
+ 			expect(gatwick.name).to.eq('Gatwick')
+ 		});
+
+ 		it('has a default capacity of 20', function() {
+ 			expect(gatwick.capacity).to.eq(20)
+ 		});
+
+ 		it('has an hangar for store planes', function() {
+ 			expect(gatwick.hangar).to.eql([])
+ 		});
+ 	});
+ }); 
